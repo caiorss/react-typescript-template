58647b5b9b202c289a61ff307be8103fec4cda1a
CaioRS <caiorss.rodrigues@gmail.com>
2021-09-23 16:38:44 -0300

    Implement itunes search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef  } from 'react';
+import List from "./components/List";
+import { ItunesTypes, Result } from "./ItunesInterfaces"
+
+interface RequestResult {
+   res: Response;
+   data: JSON;
+}
+
+// Send GET request to REST APIs
+async function request_get(url: string): Promise<RequestResult>
+{
+    const res = await fetch(url,
+                            { "method": "GET"
+                                , headers: {
+                                      'Content-Type': 'application/json'
+                                    , 'X-Requested-With': 'XMLHttpRequest'
+                                }
+                            });
+
+    let data = await res.json();
+    return {res: res, data: data};
+}
+
+interface IState{
+   results: Result[]
+}
 
 function App() {
+
+  const [results, setResults ] = useState<IState["results"]>([]);
+
+  const refSearch = useRef<HTMLInputElement>(null);
+
+  async function search(){
+    // let input = refSearch.current as any; // as HTMLInputElement;
+    let entry = refSearch.current?.value; //input === null ? "" : input.value;
+
+    let url = `https://itunes.apple.com/search?term=${entry}&entity=album`;
+    let res = await request_get(url);
+    console.log(res.data);
+    let dat =  res.data as ItunesTypes;
+    setResults(dat.results || []);
+  }
+
+  function clear(){
+    setResults([]);
+    if( refSearch.current != null){ refSearch.current.value = ""; }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Search Itunes API</h1>
+      <input ref={refSearch} type="text" />
+      <button onClick={search} >Search</button>
+      <button onClick={clear} >Clear</button>
+      <List results={results} />
+    </>
+
+  ) ;
 }
 
 export default App;
---
+++ src/components/List.tsx
@@ -0,0 +1,56 @@
+import { RSA_NO_PADDING } from "constants";
+import React from "react"
+
+import { Result } from "../ItunesInterfaces"
+const { v4: uuidv4 } = require('uuid');
+
+
+interface IProps{
+    results: Result[]
+};
+
+// React's functional component
+//
+const List: React.FC<IProps> = ( { results } ) => {
+
+    const renderList = (): JSX.Element[] => {
+        return results.map( (res: Result) => {
+            return (
+              <div key={uuidv4()}>
+                <hr></hr>
+                <table>
+                    <tr>
+                        <td>Artist</td>
+                        <td> <a href={res.artistViewUrl}>{res.artistName}</a> </td>
+                    </tr>
+                    <tr>
+                        <td>Artist ID</td>
+                        <td>{res.artistId}</td>
+                    </tr>
+                    <tr>
+                        <td>Collection</td>
+                        <td> <a href={res.collectionViewUrl}>{res.collectionName}</a> </td>
+                    </tr>
+                    <tr>
+                        <td>Genre</td>
+                        <td>{res.primaryGenreName}</td>
+                    </tr>
+                    <tr>
+                        <td>Price</td>
+                        <td>{res.collectionPrice}</td>
+                    </tr>
+                </table>
+                <img src={res.artworkUrl60} />
+            </div> 
+            )
+        });
+    };
+
+    return ( 
+       <div>
+           {renderList()}
+       </div> 
+    )
+};
+
+export default List 
